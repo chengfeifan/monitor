a2e3d85aff564f90b1dbb525d24740d795df3657
chengfeifan <chengff11@gmail.com>
2018-04-18 11:30:58 -0500

    Before the PCA model build, Scale_pca is used to center and scale the data

---
+++ R/scale_pca.R
@@ -0,0 +1,24 @@
+#' Scale function for PCA model
+#' 
+#' @description Before the data is used for PCA, the data should be centered and scaled
+#' @param normal The standard matrix is used to scale
+#' @param fault The matrix is used to be scaled. When it is null, it equals to the normal
+#' @param ... Lazy dots for additional internal arguments
+#' 
+#' @return A matrix of the scaled matrix fault
+#' 
+#' @examples 
+#' data(normal_data)
+#' normal_scale <- scale_pca(normal_data)
+#' @export
+scale_pca <- function(normal, fault = NULL,...){
+  if(is.null(fault))
+    fault <- normal
+  normal_mean <- apply(normal,2,mean)
+  normal_sd <- apply(normal,2,sd)
+  
+  fault_scale <- apply(fault, 1, function(x,normal_mean,normal_sd){
+    (x-normal_mean)/normal_sd
+  },normal_mean,normal_sd)
+  return(t(fault_scale))
+}
